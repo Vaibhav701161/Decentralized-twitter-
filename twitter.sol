fc09ee6f2a45dbff141264b8e21eba4c305d9b84
Vaibhav Mittal <Vaibhavmittal929@gmail.com>
2024-05-07 10:32:25 +0530

    added struct

--- twitter.sol
+++ twitter.sol
@@ -2,14 +2,34 @@
 pragma solidity ^0.8.25;
 
 contract twitter {
-    mapping (address => string) public tweets; //declares a mapping named tweets.
-    function createTweet(string memory _tweet) public { // defines a function named createTweet that takes a single argument of type string named _tweet.
-        tweets[msg.sender] = _tweet; //assigns the value of the _tweet argument to the key msg.sender in the tweets mapping.
+
+        struct tweet{
+             address author;
+             string content;
+             uint256 timestamp;
+             uint256 likes;
+        }
+
+    mapping (address => tweet[]) public tweets; //declares a mapping named tweets.
+    function createTweet(string memory _tweet) public {
+         tweet memory newTweet = tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,// did this since data is coming directly from the block.
+            likes:0
+         });
+
+
+        tweets[msg.sender].push(newTweet); //assigns the value of the _tweet argument to the key msg.sender in the tweets mapping.
     }
 
-    function getTweet(address _owner) view  public returns (string memory){
-        return tweets[_owner] ; 
+    function getTweet(address _owner,uint _i) view  public returns (tweet memory){
+        return tweets[_owner][_i] ; // this will get me tweet at some index i.
 
      }
+     function getAllTweets(address _owner) public view returns(tweet[] memory){
+        return tweets [_owner]; // hadn't passed index here because we want all the tweets.
+     } 
+
 
 }
